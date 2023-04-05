1c599b59acc09c5d57d01fdbee20a027a1373df2
Nikhil Bhagat <55572863+NikeGunn@users.noreply.github.com>
2023-04-05 15:30:54 +0545

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "https://ask-ai-w4g9.onrender.com,
+            "https://askai.onrender.com,
             // "http://localhost:5173",
             { input },
             {
